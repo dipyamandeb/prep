4c2a497b87fc3c1965851885857945dc0e18ccf6
dipyaman <dipyaman@dipyaman-PC>
2021-06-27 16:31:03 +0530

    Ceil implementation of number and alphabets array.

---
+++ prep/src/main/java/com/bs/alphabet/Solution.java
@@ -0,0 +1,41 @@
+package com.bs.alphabet;
+
+/*  Ciel implementation
+ *  Ceil is the smallest element which is greater than the target
+ */
+public class Solution {
+	public static void main(String[] args) {
+		char letters[] = { 'A', 'r', 'z' };
+		char target = 'z';
+		char result = nextGreatestAlphabet(letters, target);
+
+		// Function call
+		System.out.println("Next Alphabet is:" + result);
+	}
+
+	private static char nextGreatestAlphabet(char[] arr, char ele) {
+		if (arr.length == 0) {
+			return '#';
+		}
+		int start = 0;
+		int end = arr.length - 1;
+		int length = arr.length;
+		int mid = 0;
+		char res = '#';
+		while (start <= end) {
+			mid = (start + end) / 2;
+			if (ele == arr[mid]) {
+				res = arr[(mid + 1) % length];
+				return res;
+			} else if (ele < arr[mid]) {
+				res = arr[mid];
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
+		}
+
+		return res;
+	}
+
+}
--- prep/src/main/java/com/bs/floorOfElementSA/Solution.java
+++ prep/src/main/java/com/bs/floorOfElementSA/Solution.java
@@ -1,15 +1,44 @@
 package com.bs.floorOfElementSA;
 
 /*
- * Find floor of an element in a sorted array
+ * Find floor & Ceil of an element in a sorted array
  * Floor is the greatest element which is smaller then the target
+ * Ceil is the smallest element which is greater than the target
  */
 public class Solution {
 	public static void main(String[] args) {
 		int arr[] = new int[] { 1, 2, 3, 4, 8, 20, 20, 23, 29 };
-		int ele = 30;
+		int ele = 24;
 		int floorNumber = findFloorInSortedArray(arr, ele);
-		System.out.println("Index of Ele:" + floorNumber);
+		System.out.println("Floor:" + floorNumber);
+		int ceilNumber = findCeilNumber(arr,ele);
+		System.out.println("Ceil:"+ceilNumber);
+		
+	}
+
+	private static int findCeilNumber(int[] arr, int ele) {
+		if (arr.length==0) {
+			return -1;
+		}
+		
+		int res =0 ;
+		int mid = 0;
+		int start = 0;
+		int end = arr.length-1;
+		while(start<=end) {
+			mid = (start+end)/2;
+			if (ele ==arr[mid]) {
+				return arr[mid];
+			}
+			else if (ele>arr[mid]) {
+				start = mid+1;
+			}else if(ele<arr[mid]){
+				res = mid;
+				end = mid -1;
+			}
+			
+		}
+		return arr[res];
 	}
 
 	private static int findFloorInSortedArray(int[] arr, int ele) {
